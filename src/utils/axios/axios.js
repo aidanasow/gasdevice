1ec22ca00e417bf1bc58267a1182dc0d681bb60b
Aidana <aidanasoww@icloud.com>
2025-03-23 15:31:22 +0600

    add robot.txt, fix: csrf

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -1,8 +1,21 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const instance=()=> axios.create({
-    baseURL:BASE_URL
+    baseURL:BASE_URL,
+    withCredentials: true,
 });
+
+instance().interceptors.request.use((config) => {
+    const csrftoken = Cookies.get("csrftoken");
+    if (csrftoken) {
+        config.headers["X-CSRFToken"] = csrftoken;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
 
